5cf31d3ff9131912c093cd3083dd55542b26afb2
28oso <28osoblanco2020@gmail.com>
2025-04-17 20:43:34 +0900

    fix: falture of version

--- front_end/macos/Flutter/GeneratedPluginRegistrant.swift
+++ front_end/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import firebase_auth
 import firebase_core
-import flutter_webrtc
 import google_sign_in_ios
 import path_provider_foundation
 import shared_preferences_foundation
@@ -18,7 +17,6 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FlutterWebRTCPlugin.register(with: registry.registrar(forPlugin: "FlutterWebRTCPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
--- front_end/windows/flutter/generated_plugins.cmake
+++ front_end/windows/flutter/generated_plugins.cmake
@@ -5,7 +5,6 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
-  flutter_webrtc
   permission_handler_windows
   url_launcher_windows
 )
